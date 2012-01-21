316cbae16c3bf332f2fc2396c6f60cd7a610bb91
Mark J. Titorenko <mark.titorenko@alces-software.com>
2012-01-21 22:34:15 +0000

    if operating under Rails 3.2.0 or greater, define AssetMatcher#call method differently to work with new Journey routing

--- lib/rails_dev_tweaks/granular_autoload/matchers/asset_matcher.rb
+++ lib/rails_dev_tweaks/granular_autoload/matchers/asset_matcher.rb
@@ -1,28 +1,36 @@
 class RailsDevTweaks::GranularAutoload::Matchers::AssetMatcher
 
-  def call(request)
-    main_mount = request.headers['action_dispatch.routes'].set.dup.recognize(request)
-
-    # Unwind until we have an actual app
-    while main_mount != nil
-      if main_mount.kind_of? Array
-        main_mount = main_mount.first
-
-      elsif main_mount.kind_of? Rack::Mount::Route
-        main_mount = main_mount.app
-
-      elsif main_mount.kind_of? Rack::Mount::Prefix
-        # Bah, no accessor here
-        main_mount = main_mount.instance_variable_get(:@app)
-
-      # Well, we got something
-      else
-        break
+  if Rails::VERSION::STRING >= '3.2.0'
+    def call(request)
+      request.headers['action_dispatch.routes'].router.recognize(request) do |route|
+        return true if route.app.is_a?(Sprockets::Base)
       end
     end
-
-    # what do we have?
-    main_mount.kind_of? Sprockets::Base
+  else
+    def call(request)
+      main_mount = request.headers['action_dispatch.routes'].set.dup.recognize(request)
+      
+      # Unwind until we have an actual app
+      while main_mount != nil
+        if main_mount.kind_of? Array
+          main_mount = main_mount.first
+          
+        elsif main_mount.kind_of? Rack::Mount::Route
+          main_mount = main_mount.app
+          
+        elsif main_mount.kind_of? Rack::Mount::Prefix
+          # Bah, no accessor here
+          main_mount = main_mount.instance_variable_get(:@app)
+          
+          # Well, we got something
+        else
+          break
+        end
+      end
+      
+      # what do we have?
+      main_mount.kind_of? Sprockets::Base
+    end
   end
 
 end
