b7a8578073ad542d31f6551898217183751d7b8a
Ian MacLeod <ian@nevir.net>
2012-02-05 13:28:32 -0800

    Let's duck type it

--- lib/rails_dev_tweaks/granular_autoload/matchers/asset_matcher.rb
+++ lib/rails_dev_tweaks/granular_autoload/matchers/asset_matcher.rb
@@ -1,37 +1,46 @@
 class RailsDevTweaks::GranularAutoload::Matchers::AssetMatcher
 
-  if Rails::VERSION::STRING >= '3.2.0'
-    def call(request)
-      request.headers['action_dispatch.routes'].router.recognize(request) do |route|
-        return true if route.app.is_a?(Sprockets::Base)
-      end
-      false
+  def call(request)
+    route_engine = request.headers['action_dispatch.routes']
+
+    if route_engine.respond_to?(:router)
+      mounted_app = journey_find_app(route_engine.router, request)
+    else
+      mounted_app = rack_find_app(route_engine.set.dup, request)
     end
-  else
-    def call(request)
-      main_mount = request.headers['action_dispatch.routes'].set.dup.recognize(request)
-      
-      # Unwind until we have an actual app
-      while main_mount != nil
-        if main_mount.kind_of? Array
-          main_mount = main_mount.first
-          
-        elsif main_mount.kind_of? Rack::Mount::Route
-          main_mount = main_mount.app
-          
-        elsif main_mount.kind_of? Rack::Mount::Prefix
-          # Bah, no accessor here
-          main_mount = main_mount.instance_variable_get(:@app)
-          
-          # Well, we got something
-        else
-          break
-        end
-      end
-      
-      # what do we have?
-      main_mount.kind_of? Sprockets::Base
+
+    # What do we have?
+    mounted_app.is_a? Sprockets::Base
+  end
+
+  def journey_find_app(router, request)
+    router.recognize(request) do |route|
+      return route.app
     end
+  end
+
+  def rack_find_app(router, request)
+    main_mount = router.recognize(request)
+
+    # Unwind until we have an actual app
+    while main_mount != nil
+      if main_mount.is_a? Array
+        main_mount = main_mount.first
+
+      elsif main_mount.is_a? Rack::Mount::Route
+        main_mount = main_mount.app
+
+      elsif main_mount.is_a? Rack::Mount::Prefix
+        # Bah, no accessor here
+        main_mount = main_mount.instance_variable_get(:@app)
+
+      # Well, we got something
+      else
+        break
+      end
+    end
+
+    main_mount
   end
 
 end
